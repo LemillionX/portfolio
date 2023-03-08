709155a31641cc4bb38444c574f86a2159e287ca
BuildTools <unconfigured@null.spigotmc.org>
2023-03-08 15:52:01 +0100

    add project css

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,15 @@
+import '../styles/Project.css'
 import { Link } from 'react-router-dom';
+
 
 const Project = ({section, project }) => (
     <div className="portfolio-project">
-        <h1> <Link to="/">  {section} </Link> - {project.name} </h1>
-        <div className="portfolio-media">
-            <img src={project.thumbnail} alt='My Portfolio' className='portfolio-logo'  ></img>
-        </div>
-        <div className="project-description">
-            <p> {project.description} </p>
+        <h1> <Link to="/" className='project-section'>  {section}</Link> - {project.name} </h1>
+        <div className='project-content'>
+            <div>
+                <iframe src={project.media} frameborder="0" alt='My Portfolio' className="portfolio-media" ></iframe>
+            </div>
+            <div className="project-description" dangerouslySetInnerHTML={{ __html: project.description }}></div>
         </div>
     </div>
 
