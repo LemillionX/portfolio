cf59a03e727206ba122c2282e08d9ba7e04f97e0
Sammy Rasamimanana <tiana.rasamimanana@polytechnique.edu>
2023-05-15 15:58:51 +0200

    adding proficiency in toolbox

--- src/components/ToolThumbnail.js
+++ src/components/ToolThumbnail.js
@@ -1,13 +1,17 @@
-import '../styles/Project.css'
-import ContactThumbnail from './ContactThumbnail';
-import file from '../contact/contact.json';
+import '../styles/thumbnails.css'
+import {FaStar} from 'react-icons/fa'
 
-
-
-const ToolThumbnail = ({imageUrl, title, description}) => (
-    <div className="project-thumbnail">
+const ToolThumbnail = ({imageUrl, title, description, proficiency}) => (
+    <div className="tool-thumbnail">
         <img  className='thumbnail' src={imageUrl} alt={title}></img>
-        <div className='project-legend'>
+        <div className='tool-description'>
+            <p> I'm using Blender for 3D Animation and Compositing. I can also use its API to code in Python. </p>
+            <p> Proficiency : {Array.from({ length: proficiency }, (_, i) => (
+                                    <FaStar />
+                                ))}
+            </p>
+        </div>
+        <div className='tool-legend'>
             <p>{title}</p>
         </div>
     </div>
--- src/components/Toolbox.js
+++ src/components/Toolbox.js
@@ -8,7 +8,8 @@ const Toolbox = () => (
     <div>
         <Banner/>
         <div className='portfolio-toolbox'>
-            <ToolThumbnail imageUrl="./thumbnails/animation.png" title="Blender" description="Hey hey"  />
+        <ToolThumbnail imageUrl="./thumbnails/animation.png" title="Blender" description="Hey hey" proficiency={4}  />
+        <ToolThumbnail imageUrl="./thumbnails/animation.png" title="Python" description="Hey hey" proficiency={1}   />
         </div>
     </div>
 );
